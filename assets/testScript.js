38d668d8e5f47497ae20685d86c1e041e18e3971
jmloewen <jmloewen@sfu.ca>
2018-02-10 15:29:11 -0800

    css broken a bit, most of the way towards a nice voice animation.

--- assets/testScript.js
+++ assets/testScript.js
@@ -1,21 +1,34 @@
 
 function toggleThinking(){
-  $('#heart').css('animation', 'rippleYellow 0.7s linear infinite');
+  cleanHeartClasses();
+  $('.heart').css('animation', 'rippleYellow 0.7s linear infinite');
+  $("div.heart").first().addClass("heartThinking");
   //$('#heart').css('background-color', 'yellow');
 }
 function toggleListening(){
-  $('#heart').css('animation', 'rippleGreen 0.7s linear infinite');
+  cleanHeartClasses();
+  $('.heart').css('animation', 'rippleGreen 0.7s linear infinite');
+  $("div.heart").first().addClass("heartListening");
   //$('#heart').css('background-color', 'rgba(101, 255, 120, 0.8)');
 }
 function toggleSpeaking(){
-  $('#heart').css('animation', 'rippleRed 0.7s linear infinite');
+  cleanHeartClasses();
+  $('.heart').css('animation', 'rippleRed 0.7s linear infinite');
+  $("div.heart").first().addClass("heartSpeaking");
   //$('#heart').css('background-color', 'red');
 }
 function toggleStandby(){
-  $('#heart').css('animation', 'None');
+  cleanHeartClasses();
+  $('.heart').css('animation', 'None');
+  $("div.heart").first().addClass("heartStandby");
   //$('#heart').css('background-color', 'rgba(128,128,128,1)');
 }
-
-var heart =  $("div.heart").first().css("background", "black");
-alert();
+function cleanHeartClasses(){
+  $("div.heart").first().removeClass("heartStandby");
+  $("div.heart").first().removeClass("heartSpeaking");
+  $("div.heart").first().removeClass("heartThinking");
+  $("div.heart").first().removeClass("heartListening");
+  $("div.heart").first().removeClass("animation");
+}
+toggleSpeaking();
 //$("div.heart").css(background-color, "black");
