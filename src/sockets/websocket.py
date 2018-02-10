236644a740fab7de5ce6152ca4cc284be2f8bc9e
June <kimjune01@gmail.com>
2018-02-10 13:19:41 -0800

    move datedict to src, handle long-running write state

--- src/sockets/websocket.py
+++ src/sockets/websocket.py
@@ -1,7 +1,7 @@
 from tornado.websocket import WebSocketHandler
 from textProcessing.ProcessText import ProcessText
 from asynchronous.countdown import EventLoop, Countdown
-
+from datedict import Notes
 import uuid
 
 DURATION_CONST = 20
@@ -13,6 +13,7 @@ class WebSocket(WebSocketHandler):
     uuid = None
 
     state = "greeting"
+    notes = Notes()
     '''
     Crucial methods to WebSocket class
     '''
@@ -25,20 +26,30 @@ class WebSocket(WebSocketHandler):
 
     def on_message(self, str):
         print("on_message: ", str)
-        if self.state is "greeting":
+        if self.state is "ready":
             if str == "read":
-                self.write_message("now reading")
+                self.write_message("When would you like me to read?")
                 self.state = "reading"
-            elif str == "write":
-                self.write_message("now writing")
+            elif str == "write" or str == "right":
+                self.write_message("What would you like to note?")
                 self.state = "writing"
             else:
                 self.write_message("say read or write")
             return
         elif self.state is "reading":
+            # expect str to be date time
+            # when done reading, go to ready
             self.write_message("now reading. please wait")
+            self.signalReady()
         elif self.state is "writing":
-            self.write_message("now writing. please wait")
+            if str == "DONE_BUTTON":
+                self.write_message("noted.")
+                self.signalReady()
+                return
+            success = self.saveNote(str)
+            if not success:
+                self.write_message("could not write that last bit")
+
         return str
 
     def on_close(self):
@@ -47,8 +58,21 @@ class WebSocket(WebSocketHandler):
 
     def sayGreetingsAndOptions(self):
         self.write_message("Hello! Would you like to read or write?")
-        self.state = "greeting"
+        self.state = "ready"
 
+    def signalReady(self):
+        self.write_message("Would you like to read or would you like to write?")
+        self.state = "ready"
+
+    def saveNote(str):
+        if self.isLongEnough(str):
+            notes.pushNote(str)
+            return True
+        else:
+            return False
+
+    def isLongEnough(str):
+        return len(str) > 20
 
     def clearEventLoop(self):
         self.eventLoop.stop()
